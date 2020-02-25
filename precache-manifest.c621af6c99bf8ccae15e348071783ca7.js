06056e34ae00f2ff3bc53c5c037c09bcdd076cdc
Nazarsenychak <nazaesenychak1@gmail.com>
2020-02-25 11:43:26 +0200

    Updates

---
+++ precache-manifest.c621af6c99bf8ccae15e348071783ca7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba681db35f52735df8e9b21283d9a733",
+    "url": "/react-search-and-sort-data-api/index.html"
+  },
+  {
+    "revision": "f2b1038a582b28755358",
+    "url": "/react-search-and-sort-data-api/static/js/2.faf74893.chunk.js"
+  },
+  {
+    "revision": "de2007122caba8d0db0c",
+    "url": "/react-search-and-sort-data-api/static/js/main.6bd0607d.chunk.js"
+  },
+  {
+    "revision": "6d7a6ca85a87f8faa169",
+    "url": "/react-search-and-sort-data-api/static/js/runtime-main.a968a5ef.js"
+  }
+]);
